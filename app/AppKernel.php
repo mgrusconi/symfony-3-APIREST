83d0fda66851fdf5a2ca4e7ba4d79a93e9104df5
Marcelo <marcelo.rusconi-ext@dominion-global.com>
2018-05-28 15:50:35 +0200

    add nelmio_cors bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
             new Application\SomeBundle\SomeBundle(),
+            new Nelmio\CorsBundle\NelmioCorsBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
